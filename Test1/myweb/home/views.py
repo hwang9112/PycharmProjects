4ff580f45d7ec5bdc7a5963681485db4bfef0ed0
hwang9112 <43162006+hwang9112@users.noreply.github.com>
2020-01-14 17:03:42 +0900

    API TEST

--- Test1/myweb/home/views.py
+++ Test1/myweb/home/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
-
+from feedback.models import *
+from datetime import datetime
 
 def index(request):
     msg = 'My Message Test'
     return render(request, 'home/index.html', {'message': msg})
+
+
+    # # Feedback 객체 생성
+    # fb = Feedback(name='Kim', email='kim@test.com', comment='Hi', createDate=datetime.now())
+    #
+    # # 새 객체 INSERT
+    # fb.save()
+
+    for f in Feedback.objects.all():
+        s += str(f.id) + ' : ' + f.name + '\n'
+
+
+    return render(request, 'home/index.html', {'s': s})
+
