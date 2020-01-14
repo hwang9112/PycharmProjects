a78d2e82381bfb6948ee71520ab74be0427ef38c
hwang9112 <43162006+hwang9112@users.noreply.github.com>
2020-01-14 13:46:35 +0900

    Add template

--- Test1/myweb/myweb/urls.py
+++ Test1/myweb/myweb/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+   #path('admin/', admin.site.urls),
     path('', views.index, name="index")
 ]
