c84cdb80e808385b402d10ca1fbc79971e9534de
hwang9112 <43162006+hwang9112@users.noreply.github.com>
2020-01-17 12:13:05 +0900

    범위 내 랜덤 숫자 Pick 및 기록

--- PyQt_Practice/Random_Number.py
+++ PyQt_Practice/Random_Number.py
@@ -8,6 +8,7 @@ from random import randrange
 
 
 
+
 class MyApp(QWidget):
 
     def __init__(self):
@@ -19,25 +20,26 @@ class MyApp(QWidget):
         self.resulttext = ""
         self.i = 1
         self.okButton = QPushButton('굴리기',self)
-        self.cancelButton = QPushButton('취소')
+        self.cancelButton = QPushButton('기록 지우기')
         self.Start_range = QLineEdit()
         self.End_range = QLineEdit()
-        # Range_label = QLabel("범위입력",self)
+        Range_label = QLabel("범위입력",self)
         self.lcd = QLCDNumber(self)
         self.Result_List = QTextEdit()
 
         self.Result_List.setAcceptRichText(False)
-        self.Result_List.setText("11111111111111")
+        self.Result_List.setText("결과기록")
 
         self.Start_range.setText("0")
         self.End_range.setText("1")
 
         self.okButton.clicked.connect(self.okbtn_click)
-        self.cancelButton.clicked.connect(QCoreApplication.instance().quit)
+        self.cancelButton.clicked.connect(self.clear_list)
 
 
         range_box = QHBoxLayout()
         range_box.addStretch(1)
+        range_box.addWidget(Range_label)
         range_box.addWidget(self.Start_range)
         range_box.addWidget(self.End_range)
         range_box.addStretch(1)
@@ -46,31 +48,33 @@ class MyApp(QWidget):
         hbox.addStretch(1)
         hbox.addWidget(self.okButton)
         hbox.addWidget(self.cancelButton)
-        hbox.addWidget(self.Result_List)
         hbox.addStretch(1)
+
+        Result_box = QHBoxLayout()
+        Result_box.addStretch(1)
+        Result_box.addWidget(self.Result_List)
+        Result_box.addStretch(1)
+
 
         lcd_box = QHBoxLayout()
         lcd_box.addStretch(1)
         lcd_box.addWidget(self.lcd)
         lcd_box.addStretch(1)
 
-        result_box = QHBoxLayout()
-        result_box.addStretch(3)
-        result_box.addLayout(lcd_box)
-        result_box.addWidget(self.Result_List)
-        result_box.addStretch(1)
 
         vbox = QVBoxLayout()
         vbox.addStretch(1)
-        vbox.addLayout(hbox)
         vbox.addLayout(range_box)
-        vbox.addLayout(result_box)
+        vbox.addLayout(hbox)
+        vbox.addLayout(lcd_box)
+        vbox.addLayout(Result_box)
         vbox.addStretch(1)
 
         self.setLayout(vbox)
 
         self.setWindowTitle('랜덤 숫자 뽑기')
         self.setGeometry(300, 300, 300, 200)
+        self.setFixedSize(300, 300)
         self.show()
 
 
@@ -81,6 +85,10 @@ class MyApp(QWidget):
         self.Result_List.setText(self.resulttext)
         self.i += 1
 
+    def clear_list(self):
+        self.Result_List.clear()
+        self.resulttext = ""
+        self.i = 1
 
 
 if __name__ == '__main__':
